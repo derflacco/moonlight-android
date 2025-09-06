db483bf795578c7788bf4dc3f11b0613072863df
IlFlacco <paul89rulez@gmail.com>
2025-09-06 21:32:35 +0200

    set KEY_PRIORITY alongside max op-rate
    - Use the priority hint alongside the max operating rate.
    
    - Helps avoid stalls (packet losses) on picky devices.
    
    - Still only uses max op-rate where it’s supported.

--- app/src/main/java/com/limelight/binding/video/MediaCodecHelper.java
+++ app/src/main/java/com/limelight/binding/video/MediaCodecHelper.java
@@ -583,7 +583,7 @@ public class MediaCodecHelper {
                 videoFormat.setInteger(MediaFormat.KEY_OPERATING_RATE, Short.MAX_VALUE);
                 setNewOption = true;
             }
-            else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 videoFormat.setInteger(MediaFormat.KEY_PRIORITY, 0);
                 setNewOption = true;
             }
