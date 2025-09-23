4fce9773a59ea83fc8f02c3ae1d7a01d2db7d6cc
IlFlacco <paul89rulez@gmail.com>
2025-09-23 15:57:17 +0200

    fixup! Cpu affinity: cut log spam - debug-only log

--- app/src/main/java/com/limelight/binding/video/MediaCodecDecoderRenderer.java
+++ app/src/main/java/com/limelight/binding/video/MediaCodecDecoderRenderer.java
@@ -1332,7 +1332,9 @@ boolean isC2Decoder = false;
                                 if (lastAllowedMask == null || !__maskBefore.equals(lastAllowedMask)) {
                                     com.limelight.utils.CpuAffinity.pinCurrentThreadToBigCoresIf(true);
                                     String __maskAfter = com.limelight.utils.CpuAffinity.readAllowedCpuListForCurrentThread();
+                                    if (BuildConfig.DEBUG) {
                                     LimeLog.info("RendererAffinity: refresh_pin allowed_before=" + __maskBefore + " allowed_after=" + __maskAfter);
+                                    }
                                     lastAllowedMask = __maskAfter;
                                 }
                             } catch (Throwable ignored) {}
