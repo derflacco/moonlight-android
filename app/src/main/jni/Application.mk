93986c77b7b4be72c01b34b45373c497e5afc372
IlFlacco <paul89rulez@gmail.com>
2025-10-13 23:24:27 +0200

    build: -Perf: o3 / Full-Lto

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -8,3 +8,20 @@ APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 
 APP_PLATFORM := android-21
 APP_STL := c++_shared
+# === Perf-only overrides ===
+ifeq ($(APP_PERF),1)
+    ifndef APP_OPTIM
+        APP_OPTIM := release
+    endif
+
+    # (Full LTO)
+    APP_CFLAGS   += -O3 -DNDEBUG -flto -ffunction-sections -fdata-sections
+    APP_CPPFLAGS += -O3 -DNDEBUG -flto -ffunction-sections -fdata-sections
+
+    #  (Full LTO)
+    APP_LDFLAGS  += -flto -fuse-ld=lld -Wl,--icf=safe -Wl,--gc-sections
+
+
+$(info [NDK] PERF flags enabled: -O3 -flto -fuse-ld=lld)
+
+endif
