2f5e1c24b2acf8e82aab1dfbaa5c51ec0710b566
IlFlacco <paul89rulez@gmail.com>
2025-12-11 18:17:23 +0100

    latency policy: test: enable lfr for adaptx

--- app/src/main/java/com/limelight/Game.java
+++ app/src/main/java/com/limelight/Game.java
@@ -4528,7 +4528,7 @@ public class Game extends AppCompatActivity implements SurfaceHolder.Callback,
                     (fp == PreferenceConfiguration.FRAME_PACING_MIN_LATENCY);
 
             // Bypass LFR on Balanced/CapFPS/Max Smoothness and non-latency AdaptX
-            return userLfr && !isBalanced && !isCapFps && !isMaxSmooth && !isAdaptx;
+            return userLfr && !isBalanced && !isCapFps && !isMaxSmooth;
         }
 
         
