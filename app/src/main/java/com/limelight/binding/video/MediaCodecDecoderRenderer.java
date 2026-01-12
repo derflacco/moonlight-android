d31258438f597f826772be8d5b695efcdf7e0809
IlFlacco <paul89rulez@gmail.com>
2026-01-12 18:46:29 +0100

    render: bump choreo priority to urgent display

--- app/src/main/java/com/limelight/binding/video/MediaCodecDecoderRenderer.java
+++ app/src/main/java/com/limelight/binding/video/MediaCodecDecoderRenderer.java
@@ -1897,7 +1897,7 @@ try {
         if (choreographerHandlerThread == null) {
             choreographerHandlerThread = new HandlerThread(
                     "Video - Choreographer",
-                    Process.THREAD_PRIORITY_DISPLAY
+                    Process.THREAD_PRIORITY_URGENT_DISPLAY
             );
             choreographerHandlerThread.start();
             choreographerHandler = new Handler(choreographerHandlerThread.getLooper());
